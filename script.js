39b0a2fdec678796cf468a225c9ff646cbd5fba7
Aayush Arya <aayush10738@gmail.com>
2025-10-21 02:19:46 +0530

    feat: updated intro animations

--- script.js
+++ script.js
@@ -3,24 +3,27 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-var tl = gsap.timeline()
+var tl = gsap.timeline();
 
-tl.to("#page1",{
-    y:"100vh",
-    scale:0.6,
-    duration:0
-})
-tl.to("#page1",{
-    y:"10vh",
-    duration:1,
-    delay:1
-})
-tl.to("#page1",{
-    y:"0vh",
-    rotate:360,
-    scale:1,
-    duration:0.7,
-    delay:1.4
-})
-
-gsap.set("#nav", { transformOrigin: "top center" });
+tl.to("#page1", {
+    y: "100vh",
+    scale: 0.2,
+    duration: 0
+});
+tl.to("#page1", {
+    y: "10vh",
+    duration: 1,
+    delay: 1
+});
+tl.to("#page1", {
+    y: "0vh",
+    rotate: -360,
+    scale: 1,
+    duration: 1.2,
+    ease: "power2.in"
+});
+tl.to("#page1", {
+    rotate: -1080, // goes from -360 → -1080 (2 more cycles)
+    duration: 0.8, // much faster
+    ease: "power4.out" // ends fast
+});
